c3db0f7d3d80124e9cf0b3b31ead669c763c0b62
Kittykras <Silqe@hotmail.com>
2015-04-15 12:08:35 +0200

    Merge origin/master
    Conflicts:
    	src/controller/Main.java
    	src/model/CustomerBuilder.java
    	src/model/MassageBuilder.java
    	src/view/DayPanel.java
    	src/view/EditMassage.java
    	src/view/MonthPanel.java
    	src/view/ReceiptPanel.java

---
+++ src/handler/CategoryHandler.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handler;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Category;
+import util.Listeners;
+
+/**
+ *
+ * @author Annette
+ */
+public class CategoryHandler {
+    private DBConnection db;
+    private static CategoryHandler ch;
+    private Listeners listeners;
+    private String sql;
+    
+    private CategoryHandler() throws ClassNotFoundException, SQLException{
+        db = DBConnection.getInstance();
+        listeners = Listeners.getList();
+        sql = "";
+    }
+    
+    public static CategoryHandler getInstance() throws ClassNotFoundException, SQLException{
+        if(ch == null){
+            ch = new CategoryHandler();
+        }
+        return ch;
+    }
+    
+    public ArrayList<Category> getCategoryList() throws SQLException{
+        ArrayList<Category> catList = new ArrayList<>();
+        ResultSet rs;
+        rs = db.getResult("call getCategories();");
+        while(rs.next()){
+            Category category = new Category(rs.getInt("c_id"), rs.getString("c_name"));
+            catList.add(category);
+        }
+        return catList;
+    }
+}
