40a5c77be8bca431b0775e6b8beb07d903321361
marcohjeresen <markhjeresen@gmail.com>
2015-05-11 22:00:32 +0200

    Mail klasse tilføjet. MEN ikke lavet :)

--- src/util/ImageExample.java
+++ src/util/ImageExample.java
@@ -13,6 +13,7 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.io.File;
 
 import java.io.FileOutputStream;
 import java.net.URL;
@@ -20,27 +21,26 @@ import java.net.URL;
 public class ImageExample {
 
     public ImageExample() {
-//        Document document = new Document();
-//        System.out.println("hej");
-//        try {
-//            PdfWriter.getInstance(document,new FileOutputStream("Idtht.pdf"));
-//            document.open();
-//
-//            Image image1 = Image.getInstance("C:/Users/Mark/Documents/NetBeansProjects/JavaApplication1/src/pictures/event_bkgr.png");
-//            image1.setAbsolutePosition(50, 700);
-//            document.add(new Paragraph("A Hello World PDF document."));
-//            document.add(image1);
-//
-////            String imageUrl = "http://jenkov.com/images/" +
-////            "20081123-20081123-3E1W7902-small-portrait.jpg";
-////
-////            Image image2 = Image.getInstance(new URL(imageUrl));
-////        document.add(image2);
-//            document.close();
-//            System.out.println("farvel");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            System.out.println(e.getLocalizedMessage());
-//        }
+        Document document = new Document();
+        
+        System.out.println("hej");
+        try {
+            FileOutputStream file = new FileOutputStream(new File("mailfolder/", "Idtht.pdf"));
+            
+            PdfWriter.getInstance(document, file);
+            document.open();
+
+            Image image1 = Image.getInstance("C:/Users/Mark/Documents/NetBeansProjects/JavaApplication1/src/pictures/event_bkgr.png");
+            image1.setAbsolutePosition(50, 700);
+            document.add(new Paragraph("A Hello World PDF document."));
+            document.add(image1);
+
+
+            document.close();
+            System.out.println("farvel");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
---
+++ src/util/SendMail.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.nio.file.FileSystems;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.SendFailedException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+/**
+ *
+ * @author Mark
+ */
+public class SendMail {
+    
+    public SendMail(String toEmail, String PdfName) throws MessagingException {
+        String fs = FileSystems.getDefault().getSeparator();
+        String host = "smtp.gmail.com";
+        String Password = "hjzeecomh";
+        String from = "markhjeresen@gmail.com";
+        String toAddress = toEmail;
+        String filename = "." +fs+ "mailfolder" +fs+ ""+PdfName+".pdf";
+        // Get system properties
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtps.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.setRecipients(Message.RecipientType.TO, toAddress);
+        message.setSubject("JavaMail Attachment");
+        BodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setText("Here's the file");
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        messageBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+        multipart.addBodyPart(messageBodyPart);
+        message.setContent(multipart);
+
+        try {
+            Transport tr = session.getTransport("smtps");
+            tr.connect(host, from, Password);
+            tr.sendMessage(message, message.getAllRecipients());
+            System.out.println("Mail Sent Successfully");
+            tr.close();
+        } catch (SendFailedException sfe) {
+            System.out.println(sfe);
+        }
+    }
+    
+}
